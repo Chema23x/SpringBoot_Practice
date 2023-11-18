4a3221f4a4c8c514d298d4eccebfe4069ae41bb6
Chema23x <jmanuel.hernandezb23@gmail.com>
2023-11-18 12:49:42 -0600

    Funcionalidad para cargar la imagen del producto

--- src/main/java/com/practice/ecommerce/controller/ProductController.java
+++ src/main/java/com/practice/ecommerce/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.practice.ecommerce.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.practice.ecommerce.model.Product;
 import com.practice.ecommerce.model.User;
 import com.practice.ecommerce.service.ProductService;
+import com.practice.ecommerce.service.UploadFileService;
 
 @Controller
 @RequestMapping("/products")
@@ -23,6 +27,9 @@ public class ProductController {
 	
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
+	private UploadFileService upload;
 	
 	@GetMapping("")
 	public String show(Model model) {
@@ -34,10 +41,26 @@ public class ProductController {
 		return "productos/create";
 	}
 	@PostMapping("/save")
-	public String save(Product product) {
+	public String save(Product product, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es el objeto product {}", product);
 		User user= new User(1,"","","","","","",""); 
 		product.setUser(user);
+		
+		//images
+		if (product.getId()==null) { //cuando se crea un producto
+			String nameImage=upload.saveImage(file);
+			product.setImage(nameImage);
+		}else {
+			if(file.isEmpty()) { // editamos el producto pero no cambiamos la imagen
+				Product p = new Product();
+				p=productService.get(product.getId()).get();
+				product.setImage(p.getImage());
+			}else {
+				String nameImage=upload.saveImage(file);
+				product.setImage(nameImage);
+			}
+		}
+		
 		productService.save(product);
 		return "redirect:/products";
 	}
