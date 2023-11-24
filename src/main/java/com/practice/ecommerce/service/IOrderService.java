76951ddd8aed392670d2f6d71c08c4393ac3718a
Chema23x <jmanuel.hernandezb23@gmail.com>
2023-11-23 19:00:30 -0600

    Metodo para generar el numero de la orden

--- src/main/java/com/practice/ecommerce/service/IOrderService.java
+++ src/main/java/com/practice/ecommerce/service/IOrderService.java
@@ -1,7 +1,12 @@
 package com.practice.ecommerce.service;
 
+import java.util.List;
+
 import com.practice.ecommerce.model.Order;
 
 public interface IOrderService {
+	
+	List<Order> findAll();
+	
 	Order save(Order order);
 }
--- src/main/java/com/practice/ecommerce/service/OrderServiceImpl.java
+++ src/main/java/com/practice/ecommerce/service/OrderServiceImpl.java
@@ -1,5 +1,8 @@
 package com.practice.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,44 @@ public class OrderServiceImpl implements IOrderService {
 	public Order save(Order order) {
 		
 		return orderRepository.save(order);
+	}
+
+	@Override
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+	
+	// orden 000010
+	public String generateOrderNumber() {
+		
+		int numero= 0;
+		String numeroConcatenado="";
+		
+		List<Order> orders = findAll();
+		
+		List<Integer> numeros = new ArrayList<Integer>();
+		
+		orders.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumber())));
+		
+		if (orders.isEmpty()) {
+			numero=1;
+		}else{
+			numero= numeros.stream().max(Integer::compare).get();
+			numero++;
+		}
+		
+		if (numero<10) {
+			numeroConcatenado="000000000"+String.valueOf(numero);
+		} else if(numero<100) {
+			numeroConcatenado="00000000"+String.valueOf(numero);
+		} else if(numero<1000) {
+			numeroConcatenado="0000000"+String.valueOf(numero);
+		}
+		else if(numero<10000) {
+			numeroConcatenado="000000"+String.valueOf(numero);
+		}
+		
+		return numeroConcatenado;
 	}
 
 }
