cb0df854782fc3fd2304cb231dd86b92b1d40d95
Chema23x <jmanuel.hernandezb23@gmail.com>
2023-11-24 17:11:46 -0600

    funcionalidad ver detalle de compra al user

--- src/main/java/com/practice/ecommerce/service/IOrderService.java
+++ src/main/java/com/practice/ecommerce/service/IOrderService.java
@@ -1,6 +1,7 @@
 package com.practice.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.practice.ecommerce.model.Order;
 import com.practice.ecommerce.model.User;
@@ -9,9 +10,13 @@ public interface IOrderService {
 	
 	List<Order> findAll();
 	
+	Optional<Order> findById(Integer id);
+	
 	Order save(Order order);
 	
 	String generateOrderNumber();
 	
 	List<Order> findByUser (User user);
+	
+
 }
--- src/main/java/com/practice/ecommerce/service/OrderServiceImpl.java
+++ src/main/java/com/practice/ecommerce/service/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.practice.ecommerce.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -63,6 +64,12 @@ public class OrderServiceImpl implements IOrderService {
 	@Override
 	public List<Order> findByUser(User user) {
 		return orderRepository.findByUser(user);
+	}
+
+	@Override
+	public Optional<Order> findById(Integer id) {
+
+		return orderRepository.findById(id);
 	}
 
 }
